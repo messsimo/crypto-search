1a2116b2ffd4961ff5c0ebf256445697181f31c9
Daniel Mihai <danikmihai23@mail.ru>
2024-08-22 11:55:32 +0300

    Add header

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Подключение CSS -->
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <!-- Название страницы сайта -->
+    <title>Coindom - Crypto Search</title>
+</head>
+<body>
+    <!-- Подключение секций -->
+    @yield("main")
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Отображение главной страницы
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
